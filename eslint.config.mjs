38857e89eb0473bb18e001ab78c29fc66e46ed4b
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-13 13:45:56 -0700

    add flat config for eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+// import * as config from '@jakejarvis/eslint-config';
+
+export default [
+  {
+    rules: {
+      semi: 'error',
+      'prefer-const': 'error',
+    },
+  },
+  // "extends": "@jakejarvis/eslint-config",
+  // "parserOptions": {
+  //   "ecmaVersion": 2020,
+  //   "sourceType": "module";
+  // },
+  // "env": {
+  //   "node": true,
+  //   "es6": true;
+  // },
+  // "ignorePatterns": [
+  //   "vendor/**",
+  //   "*.d.ts"
+  // ];
+  // config,
+];
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,17 +1,17 @@
 /* eslint-env node, mocha */
-import path from "path";
-import { execFile } from "child_process";
-import assert from "assert";
-import { deleteAsync } from "del";
-import hugo from "../index.js";
-import { getBinPath } from "../lib/utils.js";
+import path from 'path';
+import { execFile } from 'child_process';
+import assert from 'assert';
+import { deleteAsync } from 'del';
+import hugo from '../index.js';
+import { getBinPath } from '../lib/utils.js';
 
-it("Hugo exists and runs?", async function () {
+it('Hugo exists and runs?', async function () {
   this.timeout(30000); // increase timeout to an excessive 30 seconds for CI
 
   const hugoPath = await hugo();
 
-  assert(execFile(hugoPath, ["env"], function (error, stdout) {
+  assert(execFile(hugoPath, ['env'], (error, stdout) => {
     if (error) {
       throw error;
     }
@@ -23,11 +23,15 @@ it("Hugo doesn't exist, install it instead of throwing an error", async function
   this.timeout(30000); // increase timeout to an excessive 30 seconds for CI
 
   // delete binary to ensure it's auto-reinstalled
-  await deleteAsync(path.dirname(getBinPath()));
+  try {
+    await deleteAsync(path.dirname(getBinPath()));
+  } catch (error) {
+    // ignore
+  }
 
   const hugoPath = await hugo();
 
-  assert(execFile(hugoPath, ["version"], function (error, stdout) {
+  assert(execFile(hugoPath, ['version'], (error, stdout) => {
     if (error) {
       throw error;
     }
